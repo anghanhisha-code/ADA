f077fd978fb2c7ba6fa1338c311f8ca29406685e
anghanhisha-code <anghanhisha@gmail.com>
2026-04-29 20:36:20 +0530

    Create p12.java

---
+++ p12.java
@@ -0,0 +1,38 @@
+public class LCS {
+
+    public static int lcs(String X, String Y) {
+
+        int m = X.length();
+        int n = Y.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Build dp table
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                }
+                else if (X.charAt(i - 1) == Y.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+
+        String X = "ABCBDAB";
+        String Y = "BDCAB";
+
+        int length = lcs(X, Y);
+
+        System.out.println("Length of LCS = " + length);
+    }
+}
